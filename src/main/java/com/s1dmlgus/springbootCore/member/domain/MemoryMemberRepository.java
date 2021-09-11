320fb757684a465e15a712d1008b3208acb8c4b0
t1dmlgus <dmlgusgngl@gmail.com>
2021-09-11 10:31:12 +0900

    feat: 회원 도메인 엔티티 생성

---
+++ src/main/java/com/s1dmlgus/springbootCore/member/domain/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package com.s1dmlgus.springbootCore.member.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    @Override
+    public void save(Member member) {
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return null;
+    }
+
+
+}
---
+++ src/main/java/com/s1dmlgus/springbootCore/member/service/MemberServiceImpl.java
@@ -0,0 +1,16 @@
+package com.s1dmlgus.springbootCore.member.service;
+
+import com.s1dmlgus.springbootCore.member.domain.Member;
+
+public class MemberServiceImpl implements MemberService {
+
+    @Override
+    public void join(Member member) {
+
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return null;
+    }
+}
